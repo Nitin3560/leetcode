dbb999c26b87a3b9622b2a3c812240febd0d1d0a
Nitin3560 <nxr3560@mavs.uta.edu>
2025-07-15 13:34:28 -0500

    Time: 0 ms (100%), Space: 12.4 MB (83.47%) - LeetHub

---
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -0,0 +1,23 @@
+class Solution(object):
+    def letterCombinations(self, digits):
+        if not digits:
+            return []
+
+        digit_to_char = {
+            "2": "abc", "3": "def", "4": "ghi", "5": "jkl",
+            "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"
+        }
+
+        res = []
+
+        def backtrack(index, path):
+            if index == len(digits):
+                res.append("".join(path))
+                return
+            for char in digit_to_char[digits[index]]:
+                path.append(char)
+                backtrack(index + 1, path)
+                path.pop()
+
+        backtrack(0, [])
+        return res
