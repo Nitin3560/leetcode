37d562dd69c2d9d6a07a353451ac42968c6b16b2
Nitin3560 <nxr3560@mavs.uta.edu>
2025-07-27 19:34:17 -0500

    Time: 7 ms (42.99%), Space: 18.8 MB (79.08%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.py
+++ 0283-move-zeroes/0283-move-zeroes.py
@@ -1,11 +1,20 @@
-class Solution(object):
-    def moveZeroes(self, nums):
-        insert_pos = 0
-        for num in nums:
-            if num != 0:
-                nums[insert_pos] = num
-                insert_pos += 1
-        while insert_pos < len(nums):
-            nums[insert_pos] = 0
-            insert_pos += 1
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        k=0
+        count=0
+        n=len(nums)-1
+        for i in range(len(nums)):
+            if nums[i]!=0:
+                nums[k]=nums[i]
+                k+=1
+            else:
+                count+=1
+        
+        while count>0:
+            nums[n]=0
+            n-=1
+            count-=1
+        
         return nums
+                
+        
