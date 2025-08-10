a8e1dd8bf9b68b6810dff4714ae821ecc0796021
Nitin3560 <nxr3560@mavs.uta.edu>
2025-08-10 00:10:14 -0500

    Time: 51 ms (7.36%), Space: 26.6 MB (99.37%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -1,7 +1,9 @@
-class Solution(object):
-    def containsDuplicate(self, nums):
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        if len(nums) <= 1:
+            return False
         nums.sort()
-        for i in range(len(nums)-1):
-            if nums[i]==nums[i+1]:
+        for i in range(len(nums) - 1):
+            if nums[i] == nums[i + 1]:
                 return True
         return False
