d0ccdb2432b51868ba66bcc71a8f1ceadcf2d6b4
Nitin3560 <nxr3560@mavs.uta.edu>
2025-08-13 20:35:19 -0500

    Time: 0 ms (100%), Space: 17.9 MB (31.01%) - LeetHub

---
+++ 0357-count-numbers-with-unique-digits/0357-count-numbers-with-unique-digits.py
@@ -0,0 +1,16 @@
+class Solution:
+    def countNumbersWithUniqueDigits(self, n: int) -> int:
+        if n == 0:
+            return 1
+        
+        count = 10  
+        unique = 9  
+        available = 9 
+        
+        for k in range(2, n + 1):
+            unique *= available
+            count += unique
+            available -= 1
+        
+        return count
+        
