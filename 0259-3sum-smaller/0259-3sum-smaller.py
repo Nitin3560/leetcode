72f567e392f1cb7186cbbd41d30f76381b66176f
Nitin3560 <nxr3560@mavs.uta.edu>
2025-08-20 12:13:22 -0500

    Time: 561 ms (39.44%), Space: 17.9 MB (59.3%) - LeetHub

---
+++ 0259-3sum-smaller/0259-3sum-smaller.py
@@ -0,0 +1,19 @@
+class Solution:
+    def threeSumSmaller(self, nums: List[int], target: int) -> int:
+        n = len(nums)
+        if n < 3:
+            return 0
+
+        nums.sort()
+        count = 0
+
+        for i in range(n - 2):
+            l, r = i + 1, n - 1
+            while l < r:
+                s = nums[i] + nums[l] + nums[r]
+                if s < target:
+                    count += (r - l)   
+                    l += 1
+                else:
+                    r -= 1
+        return count
