6d16a5f7d74ee0468119b8750f884fb159919747
Nitin3560 <nxr3560@mavs.uta.edu>
2025-09-01 18:32:04 -0500

    Time: 7 ms (62.54%), Space: 17.9 MB (19.85%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,9 +1,11 @@
-class Solution(object):
-    def isPalindrome(self, x):
-       x_str = str(x)
-       reversed_str = x_str[::-1]
-       if x_str == reversed_str:
-        return True
-       else:
-        return False
-        
+class Solution:
+    def isPalindrome(self, x: int) -> bool:
+        dup=x
+        result=0
+        while x>0:
+            lastdigit=x%10
+            x=x//10
+            result=result*10+lastdigit
+
+        return True if result==dup else False
+    
