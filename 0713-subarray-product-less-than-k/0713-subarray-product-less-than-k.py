3d9f05189cec3b4a406afa2c265188d1ba066c56
Nitin3560 <nxr3560@mavs.uta.edu>
2025-12-09 17:43:45 -0600

    Time: 504 ms (5.01%), Space: 20.4 MB (17.28%) - LeetHub

--- 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
+++ 0713-subarray-product-less-than-k/0713-subarray-product-less-than-k.py
@@ -3,22 +3,22 @@ class Solution:
         if k == 0:
             return 0
 
-        log_k = math.log(k)
-        prefix = [0]
+        lk = math.log(k)
+        pre = [0.0]
         for x in nums:
-            prefix.append(prefix[-1] + math.log(x))
+            pre.append(pre[-1] + math.log(x))
 
-        count = 0
-        n = len(prefix)
+        ans = 0
+        m = len(pre)
 
-        for i in range(n):
-            left, right = i + 1, n
-            while left < right:
-                mid = (left + right) // 2
-                if prefix[mid] < prefix[i] + log_k - 1e-9:
-                    left = mid + 1
+        for i in range(m):
+            lo, hi = i + 1, m
+            while lo < hi:
+                mid = (lo + hi) // 2
+                if pre[mid] < pre[i] + lk - 1e-9:
+                    lo = mid + 1
                 else:
-                    right = mid
-            count += left - i - 1
+                    hi = mid
+            ans += lo - i - 1
 
-        return count
+        return ans
