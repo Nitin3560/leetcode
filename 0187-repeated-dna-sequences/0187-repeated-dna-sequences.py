2ca12e66d9a1bb9c841e523c9e25593f251bbc31
Nitin3560 <nxr3560@mavs.uta.edu>
2025-12-11 22:35:57 -0600

    Time: 16 ms (98.41%), Space: 29.3 MB (34.27%) - LeetHub

--- 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
+++ 0187-repeated-dna-sequences/0187-repeated-dna-sequences.py
@@ -1,13 +1,17 @@
-class Solution(object):
-    def findRepeatedDnaSequences(self, s):
+class Solution:
+    def findRepeatedDnaSequences(self, s: str) -> List[str]:
+        L = 10
+        if len(s) < L:
+            return []
+
         seen = set()
         repeated = set()
 
-        for i in range(len(s) - 9):
-            snippet = s[i:i+10]  
-            if snippet in seen:
-                repeated.add(snippet)
+        for i in range(len(s) - L + 1):
+            sub = s[i:i+L]
+            if sub in seen:
+                repeated.add(sub)
             else:
-                seen.add(snippet)
+                seen.add(sub)
 
         return list(repeated)
