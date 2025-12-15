8a5808b647c8d959d93a2e60e026d108bb8766d6
Nitin3560 <nxr3560@mavs.uta.edu>
2025-12-15 16:25:01 -0600

    Time: 51 ms (76.53%), Space: 20.3 MB (93.99%) - LeetHub

---
+++ 0978-longest-turbulent-subarray/0978-longest-turbulent-subarray.py
@@ -0,0 +1,21 @@
+class Solution:
+    def maxTurbulenceSize(self, arr: List[int]) -> int:
+        n = len(arr)
+        if n == 0:
+            return 0
+
+        up = down = 1
+        ans = 1
+
+        for i in range(1, n):
+            if arr[i] > arr[i - 1]:
+                up = down + 1
+                down = 1
+            elif arr[i] < arr[i - 1]:
+                down = up + 1
+                up = 1
+            else:
+                up = down = 1
+            ans = max(ans, up, down)
+
+        return ans
