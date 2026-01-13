fefb151445995b6d9ffb16237eded0c5cc3d7d93
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-01-12 18:35:59 -0600

    Time: 0 ms (100%), Space: 19.7 MB (6.27%) - LeetHub

--- 0093-restore-ip-addresses/0093-restore-ip-addresses.py
+++ 0093-restore-ip-addresses/0093-restore-ip-addresses.py
@@ -1,23 +1,19 @@
 class Solution:
     def restoreIpAddresses(self, s: str) -> List[str]:
         res = []
-    
         def valid(part: str) -> bool:
             if len(part) > 1 and part[0] == '0':
                 return False
             return 0 <= int(part) <= 255
-    
         def dfs(i: int, parts: list):
             if len(parts) == 4:
                 if i == len(s):
                     res.append(".".join(parts))
                 return
-        
             remaining_parts = 4 - len(parts)
             remaining_chars = len(s) - i
             if remaining_chars < remaining_parts or remaining_chars > 3 * remaining_parts:
                 return
-        
             for l in range(1, 4):
                 if i + l > len(s):
                     break
@@ -26,6 +22,5 @@ class Solution:
                     dfs(i + l, parts + [seg])
                 if s[i] == '0':
                     break
-    
         dfs(0, [])
         return res
