e0a89496cef6c26e92d6339b9edfb9b6b39523b7
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-01-18 23:45:22 -0600

    Time: 3 ms (90.87%), Space: 19.7 MB (10.34%) - LeetHub

--- 0040-combination-sum-ii/0040-combination-sum-ii.py
+++ 0040-combination-sum-ii/0040-combination-sum-ii.py
@@ -2,14 +2,12 @@ class Solution:
     def combinationSum2(self, candidates: List[int], target: int) -> List[List[int]]:
         candidates.sort()
         res, path = [], []
-
         def dfs(start: int, remain: int):
             if remain == 0:
                 res.append(path.copy())
                 return
             if remain < 0:
                 return
-
             prev = None
             for i in range(start, len(candidates)):
                 x = candidates[i]
@@ -21,6 +19,5 @@ class Solution:
                 dfs(i + 1, remain - x) 
                 path.pop()
                 prev = x
-
         dfs(0, target)
         return res
