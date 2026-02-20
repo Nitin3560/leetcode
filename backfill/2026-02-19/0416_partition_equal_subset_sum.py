2b53b64dd36f8564cf562aba064a336cdf3fed29
Nitin singh Rathore <nxr3560@mavs.uta.edu>
2026-02-19 20:45:00 -0600

    Practice 0416_partition_equal_subset_sum 2026-02-19

---
+++ backfill/2026-02-19/0416_partition_equal_subset_sum.py
@@ -0,0 +1,10 @@
+class Solution:
+    def canPartition(self, nums):
+        s = sum(nums)
+        if s % 2:
+            return False
+        target = s // 2
+        dp = {0}
+        for n in nums:
+            dp |= {x + n for x in dp if x + n <= target}
+        return target in dp
---
+++ backfill/2026-02-19/README.md
@@ -0,0 +1,2 @@
+Practice 2026-02-19 0416_partition_equal_subset_sum
+Timestamp 2026-02-19T20:45:00-06:00
