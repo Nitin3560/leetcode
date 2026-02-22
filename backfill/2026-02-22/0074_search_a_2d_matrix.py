7846110b13a364433fbcd4330ed529c08fa152bf
Nitin singh Rathore <nxr3560@mavs.uta.edu>
2026-02-22 10:12:00 -0600

    Practice 0074_search_a_2d_matrix 2026-02-22 #1

---
+++ backfill/2026-02-22/0074_search_a_2d_matrix.py
@@ -0,0 +1,14 @@
+class Solution:
+    def searchMatrix(self, matrix, target):
+        m, n = len(matrix), len(matrix[0])
+        l, r = 0, m * n - 1
+        while l <= r:
+            mid = (l + r) // 2
+            v = matrix[mid // n][mid % n]
+            if v == target:
+                return True
+            if v < target:
+                l = mid + 1
+            else:
+                r = mid - 1
+        return False
---
+++ backfill/2026-02-22/README_0.md
@@ -0,0 +1,1 @@
+Practice 2026-02-22 0074_search_a_2d_matrix
