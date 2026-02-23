a6382104a4afe387d765e849c13bef78e41e7285
Nitin singh Rathore <nxr3560@mavs.uta.edu>
2026-02-22 21:05:00 -0600

    Practice 0394_decode_string 2026-02-22 #4

---
+++ backfill/2026-02-22/0394_decode_string.py
@@ -0,0 +1,18 @@
+class Solution:
+    def decodeString(self, s):
+        stack = []
+        cur = ""
+        num = 0
+        for ch in s:
+            if ch.isdigit():
+                num = num * 10 + int(ch)
+            elif ch == "[":
+                stack.append((cur, num))
+                cur = ""
+                num = 0
+            elif ch == "]":
+                prev, k = stack.pop()
+                cur = prev + cur * k
+            else:
+                cur += ch
+        return cur
---
+++ backfill/2026-02-22/README_3.md
@@ -0,0 +1,1 @@
+Practice 2026-02-22 0394_decode_string
