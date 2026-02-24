5e3239990be0f39eb021edfd54f5675466426e09
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-02-24 15:41:54 -0600

    Time: 2287 ms (5%), Space: 24.3 MB (5.27%) - LeetHub

--- 0015-3sum/0015-3sum.py
+++ 0015-3sum/0015-3sum.py
@@ -1,25 +1,29 @@
-class Solution(object):
-    def threeSum(self, nums):
-        nums.sort()
-        result = []
-    
-        for i in range(len(nums) -2):
-            if i > 0 and nums[i] == nums[i -1]:
-                continue
-            left = i + 1
-            right = len(nums) -1
-            while left<right:
-                sum_ = nums[i]+nums[left]+nums[right]
-                if sum_ == 0:
-                    result.append([nums[i],nums[left],nums[right]])
-                    while left < right and nums[left] == nums[left +1]:
-                        left += 1
-                    while left < right and nums[right] == nums[right - 1]:
-                        right-=1
-                    left+=1
-                    right-=1
-                elif sum_ < 0:
-                    left+=1
-                else:
-                    right-=1
-        return result
+# class Solution:
+#     def threeSum(self, nums: List[int]) -> List[List[int]]:
+#         n = len(nums)
+#         ans = set()
+#         for i in range(n):
+#             for j in range(i + 1, n):
+#                 for k in range(j + 1, n):
+#                     if nums[i] + nums[j] + nums[k] == 0:
+#                         result = tuple(sorted((nums[i], nums[j], nums[k])))
+#                         ans.add(result)
+
+#         return [list(t) for t in ans]
+
+
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        nums.sort() 
+        n = len(nums)
+        ans = set()
+        for i in range(n):
+            target = -nums[i]
+            seen = set()
+            for j in range(i + 1, n):
+                if target - nums[j] in seen:
+                    result = tuple((nums[i], nums[j], target - nums[j]))
+                    ans.add(result)
+                seen.add(nums[j])
+
+        return [list(t) for t in ans]
