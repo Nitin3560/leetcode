cd696826ec3add120bcf2f66cf735a5f7ecd57f2
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-03-03 20:59:56 -0600

    Time: 3797 ms (5.21%), Space: 21.5 MB (37.02%) - LeetHub

--- 0252-meeting-rooms/0252-meeting-rooms.py
+++ 0252-meeting-rooms/0252-meeting-rooms.py
@@ -1,9 +1,10 @@
-class Solution(object):
-    def canAttendMeetings(self, intervals):
-        starts = sorted([i[0] for i in intervals])
-        ends = sorted([i[1] for i in intervals])
-
-        for i in range(1, len(intervals)):
-            if starts[i] < ends[i - 1]:
-                return False
+class Solution:
+    def canAttendMeetings(self, intervals: List[List[int]]) -> bool:
+        n =len(intervals)
+        for i in range(n):
+            s1, e1 =intervals[i]
+            for j in range(i + 1, n):
+                s2, e2 =intervals[j]
+                if max(s1, s2) < min(e1, e2):
+                    return False
         return True
