11d3d3d936f1e77c047d6723c146644ff1924dd6
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-03-12 18:02:03 -0500

    Time: 8 ms (13.61%), Space: 19.2 MB (99.07%) - LeetHub

--- 0002-add-two-numbers/0002-add-two-numbers.py
+++ 0002-add-two-numbers/0002-add-two-numbers.py
@@ -12,7 +12,6 @@ class Solution:
         while l1 or l2 or carry:
             v1 = l1.val if l1 else 0
             v2 = l2.val if l2 else 0
-
             total = v1 + v2 + carry
             carry = total // 10
             digit = total % 10
