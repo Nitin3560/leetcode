79500cb394c6061cf5521bf417585fb4eac19cf1
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-04 00:20:27 -0500

    Time: 109 ms (5.84%), Space: 22.4 MB (33.32%) - LeetHub

--- 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
+++ 0080-remove-duplicates-from-sorted-array-ii/0080-remove-duplicates-from-sorted-array-ii.py
@@ -1,9 +1,14 @@
-class Solution(object):
-    def removeDuplicates(self, nums):
-        k=2
-        for i in range(2,len(nums)):
-            if nums[i]!=nums[k-2]:
-                nums[k]=nums[i]
-                k+=1
-        return k 
-
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        i=1
+        count=1
+        while i<len(nums):
+            if nums[i]==nums[i-1]:
+                count+=1
+                if count>2:
+                    nums.pop(i)
+                    i-=1
+                    count-=1
+            else:
+                count=1
+            i+=1
