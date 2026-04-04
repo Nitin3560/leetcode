61eda26f50322fb2b553dc9c5544cbae66e4dfc3
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-04 16:50:54 -0500

    Time: 0 ms (100%), Space: 21.2 MB (33.9%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,15 +1,14 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        set={}
+        candidate=None
+        count=0
         for num in nums:
-            if num in set:
-                set[num]+=1
+            if count == 0:
+                candidate = num
+            if num == candidate:
+                count += 1
             else:
-                set[num]=1
-        max_freq=0
-        max_num=0
-        for num in set:
-            if set[num]>max_freq:
-                max_freq=set[num]
-                max_num=num
-        return max_num
+                count -= 1
+        return candidate 
+  
+            
