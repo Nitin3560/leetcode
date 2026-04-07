10a56ee39cb20e1edae9d3dcc9de850110eaa3b4
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-06 20:21:13 -0500

    Time: 109 ms (5.78%), Space: 36.2 MB (97.47%) - LeetHub

--- 0456-132-pattern/0456-132-pattern.py
+++ 0456-132-pattern/0456-132-pattern.py
@@ -1,11 +1,16 @@
 class Solution:
     def find132pattern(self, nums: List[int]) -> bool:
-        num =float('-inf')
+        n =len(nums)
+        minnum =[float('inf')] * n
+        for i in range(1,n):
+            minnum[i] = min(minnum[i-1],nums[i-1])
         stack =[]
-        for x in reversed(nums):
-            if x <num:
-                return True
-            while stack and stack[-1] < x:
-                num =stack.pop()
-            stack.append(x)
+        for k in range(n):
+            while stack and nums[stack[-1]] <=nums[k]:
+                stack.pop()
+            if stack:
+                j =stack[-1]           
+                if minnum[j] <nums[k]:
+                    return True
+            stack.append(k) 
         return False
