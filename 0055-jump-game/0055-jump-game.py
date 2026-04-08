a500c99030600564d9cddd56942f82cc9e05ffb4
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-08 16:14:03 -0500

    Time: 12 ms (86.59%), Space: 20.2 MB (90.02%) - LeetHub

--- 0055-jump-game/0055-jump-game.py
+++ 0055-jump-game/0055-jump-game.py
@@ -1,20 +1,3 @@
-# class Solution:
-#     def canJump(self, nums: List[int]) -> bool:
-#         n=len(nums)
-#         if n<=2 and nums[0]>= (n-1):
-#             return True 
-#         if nums[0]==0:
-#             return False
-#         k=n-1
-
-#         ans = False 
-#         for i in range(n-2,0,-1):
-#             while ans is False and nums[i] >= (k - i):
-#                 ans = True
-#                 k-=1
-         
-#         return ans
-
 class Solution:
     def canJump(self, nums: List[int]) -> bool:
         goal = len(nums)-1 
@@ -22,3 +5,16 @@ class Solution:
             if i + nums[i]>=goal:
                 goal = i
         return goal == 0
+    #     n=len(nums)
+    #     if n<=2 and nums[0]>= (n-1):
+    #         return True 
+    #     if nums[0]==0:
+    #         return False
+    #     k=n-1
+    #     ans = False 
+    #     for i in range(n-2,0,-1):
+    #         while ans is False and nums[i] >= (k - i):
+    #             ans = True
+    #             k-=1
+    #     return ans
+
