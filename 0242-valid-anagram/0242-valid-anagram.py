a9b67488fdb8830bb36a5d1ffd778f09720b7446
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-11 17:03:39 -0500

    Time: 15 ms (43.16%), Space: 20.4 MB (11.33%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,6 +1,5 @@
-class Solution(object):
-    def isAnagram(self, s, t):
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
-        from collections import Counter
-        return Counter(s) == Counter(t)
+        return sorted(s) == sorted(t)
