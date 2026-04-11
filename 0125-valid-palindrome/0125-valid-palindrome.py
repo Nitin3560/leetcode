2c25d91d66b5ab6f1c2b517b334d625a1d799a8a
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-11 16:57:17 -0500

    Time: 7 ms (81.3%), Space: 19.6 MB (62.98%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -1,7 +1,14 @@
-class Solution(object):
-    def isPalindrome(self, s):
-        filtered = ''.join(c.lower() for c in s if c.isalnum())
-        if filtered == filtered[::-1]:
-            return True
-        else:
-            return False
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l=0
+        r=len(s)-1
+        while l<r:
+            while l<r and not s[l].isalnum():
+                l+=1
+            while r>l and not s[r].isalnum():
+                r-=1
+            if s[l].lower() != s[r].lower():
+                return False
+            l+=1
+            r-=1
+        return True
