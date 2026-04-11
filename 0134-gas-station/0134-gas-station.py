84300c7ffeeeab5a10a320505c91a1456112373f
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-11 11:29:44 -0500

    Time: 23 ms (61.68%), Space: 25.9 MB (75.83%) - LeetHub

--- 0134-gas-station/0134-gas-station.py
+++ 0134-gas-station/0134-gas-station.py
@@ -1,17 +1,13 @@
-class Solution(object):
-    def canCompleteCircuit(self, gas, cost):
-        total_tank = 0
-        current_tank = 0
-        start_index = 0
-
+class Solution:
+    def canCompleteCircuit(self, gas: List[int], cost: List[int]) -> int:
+        tank = 0
+        total =0
+        start= 0
         for i in range(len(gas)):
-            gain = gas[i] - cost[i]
-            total_tank += gain
-            current_tank += gain
-            if current_tank < 0:
-                start_index = i + 1
-                current_tank = 0
-
-        return start_index if total_tank >= 0 else -1
-
-        
+            net =gas[i] -cost[i]
+            tank += net
+            total +=net
+            if tank < 0:
+                start = i+1
+                tank = 0
+        return start if total >= 0 else -1
