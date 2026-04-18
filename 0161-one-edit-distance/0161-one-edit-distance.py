9ec3b6cdad8ff1ec74133c1b7c4c43a76dadca86
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-18 13:00:55 -0500

    Time: 0 ms (100%), Space: 19.3 MB (44.76%) - LeetHub

--- 0161-one-edit-distance/0161-one-edit-distance.py
+++ 0161-one-edit-distance/0161-one-edit-distance.py
@@ -1,23 +1,45 @@
 class Solution:
     def isOneEditDistance(self, s: str, t: str) -> bool:
-        m, n = len(s), len(t)
-        if abs(m - n) > 1:
+        # m, n = len(s), len(t)
+        # if abs(m - n) > 1:
+        #     return False
+        # if m > n:
+        #     s, t = t, s
+        #     m, n = n, m
+
+        # i = j = 0
+        # found_diff = False
+        # while i < m and j < n:
+        #     if s[i] == t[j]:
+        #         i += 1; j += 1
+        #     else:
+        #         if found_diff:
+        #             return False
+        #         found_diff = True
+        #         if m == n:
+        #             i += 1 
+        #         j += 1
+
+        # return found_diff if m == n else True
+        if abs(len(s)-len(t))>1:
             return False
-        if m > n:
-            s, t = t, s
-            m, n = n, m
-
-        i = j = 0
-        found_diff = False
-        while i < m and j < n:
-            if s[i] == t[j]:
-                i += 1; j += 1
-            else:
-                if found_diff:
+        if len(s)==len(t):
+            diff=0
+            for i in range(len(s)):
+                if s[i] != t[i]:
+                    diff+=1
+            return diff==1
+        s1=s if len(s)<len(t) else t
+        s2=s if len(s)>len(t) else t
+        i1, i2=0, 0
+        diff=0
+        while i1<len(s1) and i2<len(s2):
+            if s1[i1] != s2[i2]:
+                diff+=1
+                if diff>1:
                     return False
-                found_diff = True
-                if m == n:
-                    i += 1 
-                j += 1
-
-        return found_diff if m == n else True
+                i2+=1
+            else:
+                i1+=1
+                i2+=1
+        return True
