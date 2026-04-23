a44b8d387ef910ca682798562920b5c49324248d
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-23 09:59:02 -0500

    Time: 57 ms (74.13%), Space: 24.6 MB (15.2%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.py
@@ -7,24 +7,33 @@
 
 class Solution:
     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        def getPath(root, target):
-            if root is None:
-                return None
-            if root==target:
-                return [root]
-            left=getPath(root.left, target)
-            if left:
-                return [root]+left
-            right=getPath(root.right, target)
-            if right:
-                return [root]+right
+        # def getPath(root, target):
+        #     if root is None:
+        #         return None
+        #     if root==target:
+        #         return [root]
+        #     left=getPath(root.left, target)
+        #     if left:
+        #         return [root]+left
+        #     right=getPath(root.right, target)
+        #     if right:
+        #         return [root]+right
+        #     return None
+        # pathP=getPath(root, p)
+        # pathQ=getPath(root, q)
+        # lca=None
+        # i=0
+        # while i<len(pathP) and i<len(pathQ):
+        #     if pathP[i]==pathQ[i]:
+        #         lca=pathP[i]
+        #     i+=1
+        # return lca
+        if root is None:
             return None
-        pathP=getPath(root, p)
-        pathQ=getPath(root, q)
-        lca=None
-        i=0
-        while i<len(pathP) and i<len(pathQ):
-            if pathP[i]==pathQ[i]:
-                lca=pathP[i]
-            i+=1
-        return lca
+        if root==p or root==q:
+            return root
+        left=self.lowestCommonAncestor(root.left, p, q)
+        right=self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
