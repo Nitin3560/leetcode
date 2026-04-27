5b464e2edcd7334732f2bd64c8e39c026634298a
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-04-26 23:32:10 -0500

    Time: 0 ms (100%), Space: 19 MB (99.93%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,4 +1,12 @@
-from itertools import permutations
-class Solution(object):
-    def permute(self, nums):
-        return list(permutations(nums))
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        result = [[]]
+
+        for num in nums:
+            newresult = []
+            for perm in result:
+                for i in range(len(perm) + 1):
+                    newresult.append(perm[:i] + [num] + perm[i:])
+            result = newresult
+
+        return result
