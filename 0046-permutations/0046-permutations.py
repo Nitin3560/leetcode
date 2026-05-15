bbe63cb544dc5cacb834593dbe5d80b3e1832285
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-05-15 11:09:19 -0500

    Time: 0 ms (100%), Space: 19.5 MB (34.64%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,12 +1,25 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        result = [[]]
+        # result = [[]]
 
-        for num in nums:
-            newresult = []
-            for perm in result:
-                for i in range(len(perm) + 1):
-                    newresult.append(perm[:i] + [num] + perm[i:])
-            result = newresult
+        # for num in nums:
+        #     newresult = []
+        #     for perm in result:
+        #         for i in range(len(perm) + 1):
+        #             newresult.append(perm[:i] + [num] + perm[i:])
+        #     result = newresult
 
+        # return result
+        result = []
+
+        def backtrack(start):
+            if start == len(nums):
+                result.append(nums[:])
+                return
+            for i in range(start, len(nums)):
+                nums[start], nums[i] = nums[i], nums[start]
+                backtrack(start + 1)
+                nums[start], nums[i] = nums[i], nums[start] 
+
+        backtrack(0)
         return result
