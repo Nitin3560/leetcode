d66ebe39a0c297d19bc829801065807159415edf
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-06-08 13:33:43 -0500

    Time: 26 ms (59.4%), Space: 33.5 MB (48.6%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.py
@@ -5,16 +5,21 @@ class Solution:
         #     for j in range(i+1, n):
         #         if nums[i] == nums[j]:
         #             return nums[i]
-        slow= nums[0]
-        fast =nums[0]
-        while True:
-            slow =nums[slow]
-            fast =nums[nums[fast]]
-            if slow== fast:
-                break
+        
         slow = nums[0]
+        fast = nums[0]
+
+        while True:
+            slow = nums[slow]
+            fast = nums[nums[fast]]
+
+            if slow == fast:
+                break
+
+        slow = nums[0]
+
         while slow != fast:
-            slow =nums[slow]
-            fast =nums[fast]
+            slow = nums[slow]
+            fast = nums[fast]
 
         return slow
