fb731d66102bffa04e1f05040843ca417a673ab9
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-06-23 22:25:03 -0500

    Time: 43 ms (88.89%), Space: 35.7 MB (55.56%) - LeetHub

--- 3837-delayed-count-of-equal-elements/3837-delayed-count-of-equal-elements.py
+++ 3837-delayed-count-of-equal-elements/3837-delayed-count-of-equal-elements.py
@@ -1,14 +1,27 @@
 class Solution:
     def delayedCount(self, nums: List[int], k: int) -> List[int]:
+        # n = len(nums)
+        # ans = [0] * n
+        # freq = defaultdict(int)
+
+        # for i in range(n - 1, -1, -1):
+        #     j = i + k + 1
+        #     if j < n:
+        #         freq[nums[j]] += 1
+
+        #     ans[i] = freq[nums[i]]
+
+        # return ans
         n = len(nums)
         ans = [0] * n
-        freq = defaultdict(int)
+        freq = {}
 
         for i in range(n - 1, -1, -1):
             j = i + k + 1
-            if j < n:
-                freq[nums[j]] += 1
 
-            ans[i] = freq[nums[i]]
+            if j < n:
+                freq[nums[j]] = freq.get(nums[j], 0) + 1
+
+            ans[i] = freq.get(nums[i], 0)
 
         return ans
