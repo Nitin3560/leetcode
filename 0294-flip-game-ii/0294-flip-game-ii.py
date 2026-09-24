ed0fd6378bbee6f31d3b020d1af2d35df155bf35
Nitin Singh Rathore <nxr3560@mavs.uta.edu>
2026-09-23 21:00:26 -0500

    Time: 10 ms (56.41%), Space: 19.7 MB (30.77%) - LeetHub

---
+++ 0294-flip-game-ii/0294-flip-game-ii.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canWin(self, currentState: str) -> bool:
+        memo = {}
+
+        def dfs(state):
+            if state in memo:
+                return memo[state]
+
+            for i in range(len(state) - 1):
+                if state[i:i + 2] == "++":
+                    next_state = state[:i] + "--" + state[i + 2:]
+
+                    if not dfs(next_state):
+                        memo[state] = True
+                        return True
+
+            memo[state] = False
+            return False
+
+        return dfs(currentState)
